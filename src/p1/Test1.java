dbc47fc6d2948d77f0306afffa8d6d489db1cd24
Ankita Sambhare <ankitasambhare94@gmail>
2024-04-06 11:53:27 +0530

    Test1

--- src/p1/Test1.java
+++ src/p1/Test1.java
@@ -1,9 +1,9 @@
 package p1;
 
 public class Test1 {
-
-	public static void main(String[] args) {
+public static void main(String[] args) {
 		System.out.println("In Test1");
 		System.out.println("Changes in line 6");
+		System.out.println("Changes in line 7");
 	}
 }
